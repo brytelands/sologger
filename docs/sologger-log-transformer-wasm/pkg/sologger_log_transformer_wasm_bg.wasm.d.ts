fd754a0e3bd63040733582f073afb6eb49ef0eae
Will Kennedy <will.n.kennedy@gmail.com>
2024-11-20 12:08:37 -0500

    Add documentation assets and error page
    This commit introduces several new assets into the documentation, including images and WebAssembly files. Additionally, an error page and an index page have been added to enhance the user experience.

---
+++ docs/sologger-log-transformer-wasm/pkg/sologger_log_transformer_wasm_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmlogcontexttransformer_free(a: number, b: number): void;
+export function wasmlogcontexttransformer_new(a: number, b: number): number;
+export function wasmlogcontexttransformer_from_rpc_response(a: number, b: number, c: number): void;
+export function wasmlogcontexttransformer_from_rpc_logs_response(a: number, b: number, c: number, d: number): void;
+export function __wbg_wasmlogparser_free(a: number, b: number): void;
+export function wasmlogparser_new(a: number, b: number): number;
+export function wasmlogparser_parse_logs(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): number;
+export function parse_logs_basic(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
